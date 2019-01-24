4ffe994fba3b5b032249d86b5f814a279de6ae5f
Siddhartha <siddhartha047@gmail.com>
2019-01-24 14:52:08 -0500

    local main changes

--- system/main.c
+++ system/main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*  main.c  - main */
 
 #include <xinu.h>
@@ -23,29 +22,3 @@ process	main(void)
 	return OK;
     
 }
-=======
-/*  main.c  - main */
-
-#include <xinu.h>
-
-process	main(void)
-{
-
-	/* Run the Xinu shell */
-
-	recvclr();
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	while (TRUE) {
-		receive();
-		sleepms(200);
-		kprintf("This is sid");
-		kprintf("\n\nMain process recreating shell\n\n");
-		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
-	}
-	return OK;
-    
-}
->>>>>>> 59cf89868e671494f440583f1f363906cfd6e807
